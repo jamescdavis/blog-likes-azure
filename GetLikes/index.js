e48339a7baf88a6940517ec4e65c644b59790403
Chris Garrett <cgarrett@linkedin.com>
2019-01-21 21:46:09 -0800

    return 0 likes when no record exists

--- GetLikes/index.js
+++ GetLikes/index.js
@@ -16,12 +16,13 @@ module.exports = function (context, req) {
 
   // return item with RowKey 'id'
   tableService.retrieveEntity(tableName, 'Partition', postName, function (error, result, response) {
-    if (!error) {
-      context.res.status(201).json({
-        likes: result.likes._
-      });
-    } else {
+    if (error && error.statusCode !== 404) {
       context.res.status(500).json({ error: error });
+      context.done();
     }
+
+    let likes = result ? result.likes._ : 0;
+
+    context.res.status(201).json({ likes });
   });
 };
